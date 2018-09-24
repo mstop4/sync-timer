50906b9f56a476a349d304e0f9512f9ec106e446
mstop4 <mstop4@gmail.com>
2018-09-24 13:57:12 -0400

    basic tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,32 @@
 const { expect } = require("chai");
 
 describe("Index Testing", () => {
-  it("should run", async () => {
-    console.log(await browser.version());
-    expect(true).to.be.true;
+  let page;
+
+  before (async () => {
+    page = await browser.newPage();
+    await page.goto('http://localhost:8080');
   });
+
+  after (async () => {
+    await page.close();
+  })
+
+  it('should have the correct page title', async () => {
+    expect(await page.title()).to.eql('Sync Timer');
+  });
+
+  it('should only have one timer display', async () => {
+    await page.waitFor('body');
+    expect(await page.$$('#timer-display')).to.have.lengthOf(1);
+  })
+
+  it('should have a timer that reads 00:00:00', async () => {
+    let display;
+
+    await page.waitFor('#timer-display');
+    display = await page.$eval('#timer-display', timer => timer.innerText);
+
+    expect(display).to.eql('00:00:00');
+  })
 });
