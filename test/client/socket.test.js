081c2ee390ef8cfbb2d586fe36cdd0e17ad84715
mstop4 <mstop4@gmail.com>
2018-09-28 15:57:27 -0400

    fixed socketUrl in socket client test 2

--- test/client/socket.test.js
+++ test/client/socket.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const io = require('socket.io-client');
 
-const socketUrl = `http://localhost: + ${(process.env.PORT_TEST || 3001)}`;
+const socketUrl = `http://localhost:${(process.env.PORT_TEST || 3001)}`;
 const options = {
   transports: ['websocket'],
   forceNew: true
