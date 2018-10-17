d21c4d2a5de681a6061da17d3266bf794188a940
mstop4 <mstop4@gmail.com>
2018-10-16 23:13:30 -0400

    display users list per timer in admin dashboard

--- public/js/adminController.js
+++ public/js/adminController.js
@@ -11,19 +11,26 @@ var updateDisplay = function (data) {
     timerTableEl.removeChild(timerTableEl.childNodes[0]);
   }
 
+  // Table Heading
+
   var timerIdTH = document.createElement('th');
   timerIdTH.innerText = 'ID';
   var timerStateTH = document.createElement('th');
   timerStateTH.innerText = 'State';
   var timerTimeTH = document.createElement('th');
   timerTimeTH.innerText = 'Time';
+  var timerUsersTH = document.createElement('th');
+  timerUsersTH.innerText = 'Users';
 
   var timerHeadTR = document.createElement('tr');
   timerHeadTR.appendChild(timerIdTH);
   timerHeadTR.appendChild(timerStateTH);
   timerHeadTR.appendChild(timerTimeTH);
+  timerHeadTR.appendChild(timerUsersTH);
 
   timerTableEl.appendChild(timerHeadTR);
+
+  // Table Body
 
   for (var timer in data.timerList) {
     var curTimer = data.timerList[timer];
@@ -52,9 +59,20 @@ var updateDisplay = function (data) {
     var timerTimeTD = document.createElement('td');
     timerTimeTD.innerText = padDisplay(curTimer.hours, 2) + ':' + padDisplay(curTimer.minutes, 2) + ':' + padDisplay(curTimer.seconds, 2);
 
+    var timerUsersTD = document.createElement('td');
+    var timerUsersUl = document.createElement('ul');
+    timerUsersTD.appendChild(timerUsersUl);
+
+    for (var i = 0; i < curTimer.clients.length; i++) {
+      var userEntry = document.createElement('li');
+      userEntry.innerText = curTimer.clients[i];
+      timerUsersUl.appendChild(userEntry);
+    }
+
     timerTR.appendChild(timerIdTD);
     timerTR.appendChild(timerStateTD);
     timerTR.appendChild(timerTimeTD);
+    timerTR.appendChild(timerUsersTD);
     timerTableEl.appendChild(timerTR);
   }
 }
