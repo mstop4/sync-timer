54ade2883f3ec94b4c716db79e039588fd109395
mstop4 <mstop4@gmail.com>
2018-10-18 21:49:58 -0400

    style admin dashboard

--- public/js/adminController.js
+++ public/js/adminController.js
@@ -1,81 +1,62 @@
 'use strict';
 
-var timerTableEl = document.getElementById('timerTable');
+var tableBodyEl;
 
 var getDisplayElements = function() {
+  tableBodyEl = document.getElementsByTagName('tbody')[0];
   initialize();
 }
 
 var updateDisplay = function (data) {
-  while (timerTableEl.childNodes[0]) {
-    timerTableEl.removeChild(timerTableEl.childNodes[0]);
-  }
-
-  // Table Heading
-
-  var timerIdTH = document.createElement('th');
-  timerIdTH.innerText = 'ID';
-  timerIdTH.classList.add('narrow-column');
-  var timerStateTH = document.createElement('th');
-  timerStateTH.innerText = 'State';
-  timerStateTH.classList.add('narrow-column');
-  var timerTimeTH = document.createElement('th');
-  timerTimeTH.innerText = 'Time';
-  timerTimeTH.classList.add('narrow-column');
-  var timerUsersTH = document.createElement('th');
-  timerUsersTH.innerText = 'Users';
-
-  var timerHeadTR = document.createElement('tr');
-  timerHeadTR.appendChild(timerIdTH);
-  timerHeadTR.appendChild(timerStateTH);
-  timerHeadTR.appendChild(timerTimeTH);
-  timerHeadTR.appendChild(timerUsersTH);
-
-  timerTableEl.appendChild(timerHeadTR);
-
-  // Table Body
-
-  for (var timer in data.timerList) {
-    var curTimer = data.timerList[timer];
-
-    var timerTR = document.createElement('tr');
-
-    var timerIdTD = document.createElement('td');
-    timerIdTD.innerText = curTimer.id;
-
-    var timerStateTD = document.createElement('td');
-
-    switch (curTimer.timerRunning) {
-      case TIMERSTATE.STOPPED:
-        timerStateTD.innerText = 'Stopped';
-        break;
-
-      case TIMERSTATE.SUSPENDED:
-        timerStateTD.innerText = 'Suspended';
-        break;
-    
-      case TIMERSTATE.RUNNING:
-        timerStateTD.innerText = 'Running';
-        break;
+  if (tableBodyEl) {
+    while (tableBodyEl.hasChildNodes()) {
+      tableBodyEl.removeChild(tableBodyEl.lastChild);
     }
 
-    var timerTimeTD = document.createElement('td');
-    timerTimeTD.innerText = padDisplay(curTimer.hours, 2) + ':' + padDisplay(curTimer.minutes, 2) + ':' + padDisplay(curTimer.seconds, 2);
+    // Table Body
 
-    var timerUsersTD = document.createElement('td');
-    var timerUsersUl = document.createElement('ul');
-    timerUsersTD.appendChild(timerUsersUl);
+    for (var timer in data.timerList) {
+      var curTimer = data.timerList[timer];
 
-    for (var i = 0; i < curTimer.clients.length; i++) {
-      var userEntry = document.createElement('li');
-      userEntry.innerText = curTimer.clients[i];
-      timerUsersUl.appendChild(userEntry);
+      var timerTR = document.createElement('tr');
+
+      var timerIdTD = document.createElement('td');
+      timerIdTD.innerText = curTimer.id;
+
+      var timerStateTD = document.createElement('td');
+
+      switch (curTimer.timerRunning) {
+        case TIMERSTATE.STOPPED:
+          timerStateTD.innerText = 'Stopped';
+          break;
+
+        case TIMERSTATE.SUSPENDED:
+          timerStateTD.innerText = 'Suspended';
+          break;
+      
+        case TIMERSTATE.RUNNING:
+          timerStateTD.innerText = 'Running';
+          break;
+      }
+
+      var timerTimeTD = document.createElement('td');
+      timerTimeTD.innerText = padDisplay(curTimer.hours, 2) + ':' + padDisplay(curTimer.minutes, 2) + ':' + padDisplay(curTimer.seconds, 2);
+
+      var timerUsersTD = document.createElement('td');
+      var timerUsersUl = document.createElement('ul');
+      timerUsersTD.appendChild(timerUsersUl);
+
+      for (var i = 0; i < curTimer.clients.length; i++) {
+        var userEntry = document.createElement('li');
+        userEntry.innerText = curTimer.clients[i];
+        timerUsersUl.appendChild(userEntry);
+      }
+
+      timerTR.appendChild(timerIdTD);
+      timerTR.appendChild(timerStateTD);
+      timerTR.appendChild(timerTimeTD);
+      timerTR.appendChild(timerUsersTD);
+      tableBodyEl.appendChild(timerTR);
     }
-
-    timerTR.appendChild(timerIdTD);
-    timerTR.appendChild(timerStateTD);
-    timerTR.appendChild(timerTimeTD);
-    timerTR.appendChild(timerUsersTD);
-    timerTableEl.appendChild(timerTR);
   }
 }
